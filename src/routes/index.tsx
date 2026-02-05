e527023fe9e3ea50b787a6e98752e40f9882fc87
tvhfrks12678 <tvhfrks12678@gmail.com>
2026-02-05 19:47:54 +0900

    feat(ui): add card and badge from shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ import { Checkbox } from "@/components/ui/checkbox"
 import { Tilt } from 'components/motion-primitives/tilt';
 import { Label } from '@/components/ui/label';
 import { Field, FieldContent, FieldLabel, FieldTitle } from '@/components/ui/field';
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+
 // import {
 //   Zap,
 //   Server,
@@ -19,15 +22,43 @@ export const Route = createFileRoute('/')({ component: App })
 function App() {
   return (
     <div>
-      <Button>Click me</Button>
+      {/* <Button>Click me</Button>
       <Checkbox />
       <TiltCard1 />
       <div className="flex gap-2">
         <Checkbox id="terms" />
         <Label htmlFor="terms">Accept terms and conditions</Label>
-      </div>
+      </div> */}
+      <br></br>
+      <CardImage />
     </div>
   );
+}
+
+export function CardImage() {
+  return (
+    <Card className="relative mx-auto w-full max-w-sm pt-0">
+      <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+      <img
+        src="https://images.beta.cosmos.so/f7fcb95d-981b-4cb3-897f-e35f6c20e830?format=jpeg"
+        alt="Event cover"
+        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+      />
+      <CardHeader>
+        <CardAction>
+          <Badge variant="secondary">Featured</Badge>
+        </CardAction>
+        <CardTitle>Design systems meetup</CardTitle>
+        <CardDescription>
+          A practical talk on component APIs, accessibility, and shipping
+          faster.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full">View Event</Button>
+      </CardFooter>
+    </Card>
+  )
 }
 
 
