b53682210a3e8de1847a69d8fea45a8bf0b142da
tvhfrks12678 <tvhfrks12678@gmail.com>
2026-02-06 17:21:16 +0900

    feat(ui): add gradient heading component(cult ui)

--- src/components/RhymeQuizHeader.tsx
+++ src/components/RhymeQuizHeader.tsx
@@ -1,11 +1,24 @@
 import { ComicText } from "@/components/ui/comic-text"
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler"
+import { GradientHeading } from "@/components/ui/gradient-heading"
+
 
 export default function RhymeQuizHeader() {
   return (
     <div>
       <ComicText>Rhyme Quiz</ComicText>
+
+      <GradientHeadingExample />
       <AnimatedThemeToggler />
     </div>
   );
+}
+
+
+function GradientHeadingExample() {
+  return (
+    <GradientHeading variant="default" size="xs" weight="bold">
+      Rhyme Quiz
+    </GradientHeading>
+  )
 }
