9ced6668b84826a8dd8ebf4d6dca5a77783e3bf4
tvhfrks12678 <tvhfrks12678@gmail.com>
2026-02-11 17:58:29 +0900

    feat(ui): add text highlighter componet(magic ui)

--- src/features/quiz/presentation/parts/QuizQuestionCard.tsx
+++ src/features/quiz/presentation/parts/QuizQuestionCard.tsx
@@ -1,10 +1,32 @@
 
-
+import { Highlighter } from "@/components/ui/highlighter"
 
 export default function QuizQuestionCard() {
     return (
         <div>
             <h1>QuizQuestionCard</h1>
+            <HighlighterDemo />
+            <br></br>
         </div>
     );
+}
+
+
+
+export function HighlighterDemo() {
+  return (
+    <div className="text-center">
+      <p className="leading-relaxed">
+        The{" "}
+        <Highlighter action="underline" color="#FF9800">
+          Magic UI Highlighter
+        </Highlighter>{" "}
+        makes important{" "}
+        <Highlighter action="highlight" color="#87CEFA">
+          text stand out
+        </Highlighter>{" "}
+        effortlessly.
+      </p>
+    </div>
+  )
 }
