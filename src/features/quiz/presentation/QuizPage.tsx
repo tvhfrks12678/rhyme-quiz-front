3dfdd9f608cf0d7f36031b771fb9bdfe981a4d09
tvhfrks12678 <67419083+tvhfrks12678@users.noreply.github.com>
2026-02-23 13:43:11 +0900

    Adjust header layout with centered title and larger theme toggle

--- src/features/quiz/presentation/QuizPage.tsx
+++ src/features/quiz/presentation/QuizPage.tsx
@@ -1,13 +1,9 @@
-import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 import QuizQuestionCard from "./parts/QuizQuestionCard";
 
 export default function QuizPage() {
     return (
         <div>
             <QuizQuestionCard />
-            <div className="flex justify-center mb-5">
-                <AnimatedThemeToggler />
-            </div>
         </div>
     );
 }
